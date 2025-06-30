a29ce2d78afa177342d66261115680d5fffb7534
Armstrong Jonathan Fernandes <armiefdes@gmail.com>
2025-06-30 13:43:03 +0530

    Update form.tsx

--- frontend/components/form.tsx
+++ frontend/components/form.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 
-const BACKEND_URL = 'http://localhost:8080'
+const BACKEND_URL = 'https://todo-production-f715.up.railway.app'
 
 const AddTodo = ({setTodos}: {setTodos: React.Dispatch<React.SetStateAction<never[]>>}) => {
     const [form, setForm] = useState({
