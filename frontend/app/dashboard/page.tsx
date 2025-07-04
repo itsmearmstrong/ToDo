afeea9181e90f6a6c4077b3d023661681ce103f9
Armstrong Jonathan Fernandes <armiefdes@gmail.com>
2025-07-05 01:33:37 +0530

    Update page.tsx

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Card from '@/components/card';
 import AddTodo from '@/components/form';
 import CardLoader from '@/components/CardLoader';
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL;
+const BACKEND_URL = 'https://todo-production-f715.up.railway.app'
 
 const Dashboard = () => {
   const [todos, setTodos] = useState<Array<{ id: string; title: string; description: string }>>([]);
