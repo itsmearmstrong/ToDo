4a4b963485ad5977852209761038aad0e4d6862b
itsmearmstrong <armiefdes@gmail.com>
2025-07-08 02:45:23 +0530

    next

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Card from '@/components/card';
 import AddTodo from '@/components/form';
 import CardLoader from '@/components/CardLoader';
 
-const BACKEND_URL = 'http://localhost:8080'
+const BACKEND_URL = 'http://localhost:8080' 
 
 const Dashboard = () => {
   const [todos, setTodos] = useState<Array<{ id: string; title: string; description: string }>>([]);
